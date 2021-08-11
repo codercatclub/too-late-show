ccba776dbee6aff77398b364b29bd3c718b54a93
kif11 <kovalewskiy@gmail.com>
2021-08-11 21:38:10 +0600

    Added muscles and some PBR shading.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-import { extend, World, newComponent, newEntity } from "./ecs/index";
+import { extend, World, newComponent } from "./ecs/index";
 import { RenderSystem } from "./systems/core/RenderSystem";
 import { Object3DSystem } from "./systems/core/Object3DSystem";
 import { AssetManager } from "./ecs/assetManager";
-import {
-  Asset,
-  Camera,
-  HemisphereLight,
-  StandardPrimitive,
-} from "./ecs/archetypes";
+import { Asset, HemisphereLight, StandardPrimitive } from "./ecs/archetypes";
 import { AssetSystem } from "./systems/core/AssetSystem";
 import { PointLightSystem } from "./systems/core/PointLightSystem";
 import { Vector3, Color } from "three";
 import { CameraSystem } from "./systems/core/CameraSystem";
-import {
-  AnimationC,
-  CCMaterialC,
-  GLTFCameraC,
-  NeuronCoreC,
-} from "./ecs/components";
+import { AnimationC, GLTFCameraC, NeuronCoreC } from "./ecs/components";
 import { MaterialSystem } from "./systems/MaterialSystem";
 import { HemisphereLightSystem } from "./systems/core/HemisphereLightSystem";
 import { OrbitControlsSystem } from "./systems/core/OrbitControlsSystem";
@@ -29,36 +19,13 @@ import { NeuronCoreSystem } from "./systems/NeuronCoreSystem";
 import { GLTFCameraSystem } from "./systems/core/GLTFCameraSystem";
 import { AnimationSystem } from "./systems/core/AnimationSystem";
 
-interface Neuron {
-  src: string;
-  video: string;
-}
-
-const Neuron = ({ src, video }: Neuron) => [
-  extend(
-    Asset({
-      src,
-      part: "/Scene/Core",
-      scale: new Vector3(10, 10, 10),
-    }),
-    [newComponent(NeuronCoreC, { video })]
-  ),
-  Asset({
-    src,
-    part: "/Scene/Branches",
-    scale: new Vector3(10, 10, 10),
-  }),
-];
-
 (async () => {
   const assetManager = new AssetManager();
 
   assetManager
-    .addAsset("assets/models/prop_net.glb", "prop_net")
-    .addAsset("assets/models/neuron.glb", "neuron")
-    .addAsset("assets/models/neuron2.glb", "neuron")
     .addAsset("assets/models/clusters.glb", "clusters")
     .addAsset("assets/models/cameras.glb", "cameras")
+    .addAsset("assets/models/muscles.glb", "muscles")
     .addAsset("assets/textures/env.jpg", "env_tex"); // Environmental texture for PBR material.
 
   // Wait until all assets are loaded
@@ -73,9 +40,15 @@ const Neuron = ({ src, video }: Neuron) => [
     [GLTFCameraC, newComponent(AnimationC, { clipName: "CameraAction.001" })]
   );
 
-  const clusters = Asset({
-    src: "assets/models/clusters.glb",
-    debug: true,
+  const clusters = extend(
+    Asset({
+      src: "assets/models/clusters.glb",
+    }),
+    [newComponent(NeuronCoreC)]
+  );
+
+  const muscles = Asset({
+    src: "assets/models/muscles.glb",
   });
 
   const hLight = HemisphereLight({ intensity: 1 });
@@ -85,17 +58,15 @@ const Neuron = ({ src, video }: Neuron) => [
   world
     .addEntity(cameras)
     .addEntity(clusters)
-    // .addEntity(neurons)
-    // .addEntity(neuron2)
+    .addEntity(muscles)
     .addEntity(hLight)
     .addEntity(cube);
-  // .addEntities(neuron);
 
   world
     .registerSystem(
       RenderSystem.configure({
         enableShadows: false,
-        fog: { enabled: true, color: new Color(0xc2d1d1), density: 0.02 },
+        fog: { enabled: true, color: new Color(0x000000), density: 0.02 },
       })
     )
     .registerSystem(Object3DSystem)
--- src/systems/NeuronCoreSystem.ts
+++ src/systems/NeuronCoreSystem.ts
@@ -1,8 +1,15 @@
 import { System } from "../ecs/index";
-import { TransformC, MovingC, Object3DC, NeuronCoreC } from "../ecs/components";
+import { TransformC, Object3DC, NeuronCoreC } from "../ecs/components";
 import { applyQuery } from "../ecs/index";
 import { getComponent } from "./core/utils";
-import { Mesh, MeshBasicMaterial, VideoTexture } from "three";
+import {
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  MeshPhysicalMaterial,
+  Object3D,
+  VideoTexture,
+} from "three";
 
 interface NeuronCoreSystem extends System {}
 
@@ -15,33 +22,48 @@ export const NeuronCoreSystem: NeuronCoreSystem = {
     this.entities = applyQuery(world.entities, this.queries);
 
     this.entities.forEach((ent, i) => {
-      const { video: videoSrc } = getComponent(ent, NeuronCoreC);
       const { object3d } = getComponent(ent, Object3DC);
 
-      var videoEl = document.createElement("video");
+      const cores: Object3D[] = [];
 
-      videoEl.src = videoSrc;
-      // videoEl.muted = true;
-      // videoEl.autoplay = true;
-      videoEl.loop = true;
-
-      window.addEventListener('keypress', (e) => {
-        if (e.code === "KeyP") {
-          videoEl.play();
+      object3d.traverse((obj) => {
+        if (obj.name.startsWith("core")) {
+          cores.push(obj);
         }
       });
 
-      const mat = new MeshBasicMaterial();
-      const texture = new VideoTexture( videoEl )
+      cores.forEach((core) => {
+        var videoEl = document.createElement("video");
 
-      mat.map = texture;
+        videoEl.src = core.userData.videoSrc;
+        // videoEl.muted = true;
+        // videoEl.autoplay = true;
+        videoEl.loop = true;
 
-      object3d.traverse(obj => {
-        if (obj.type === "Mesh") {
-          const o = obj as Mesh;
-          o.material = mat;
-        }
-      })
+        const texture = new VideoTexture(videoEl);
+
+        window.addEventListener("keypress", (e) => {
+          if (e.code === "KeyP") {
+            videoEl.play();
+          }
+        });
+
+        texture.flipY = false;
+
+        core.traverse((obj) => {
+          if (obj.type === "Mesh") {
+            const o = obj as Mesh;
+            const mat = (o.material as MeshPhysicalMaterial).clone();
+            
+            mat.roughness = 0.3;
+            mat.metalness = 1;
+            mat.reflectivity = 1;
+            mat.emissiveMap = texture;
+
+            o.material = mat;
+          }
+        });
+      });
     });
   },
 
